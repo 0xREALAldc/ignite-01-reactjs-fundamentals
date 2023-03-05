4ad7484c88c7562cc26c31e34384dc3d646bfd5a
0xREALaldc <andredonat@hotmail.com>
2023-03-05 11:00:01 -0300

    Add: updated the imports because we added a folder to each component, created a app.module.css file, created the structure for the feed using a wrapper, aside and main tags.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { Header } from './components/Header';
+import { Header } from './components/Header/Header';
 import Post from './Post';
 
+import styles from './App.module.css'
 import './global.css'
 
 export function App() {
@@ -10,11 +11,24 @@ export function App() {
     <div>
       <Header />
       
-      <Post 
-      // in our components we can pass for it what is called 'properties', that you can see kind like 'attributes' in html
-        author="0xREALaldc" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, illum harum perspiciatis sequi facilis vero impedit repellendus placeat. Aspernatur praesentium ea doloribus veniam dignissimos asperiores consectetur voluptate repudiandae, eaque amet."
-      />
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+
+        <main>
+          <Post 
+          // in our components we can pass for it what is called 'properties', that you can see kind like 'attributes' in html
+            author="0xREALaldc" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, illum harum perspiciatis sequi facilis vero impedit repellendus placeat. Aspernatur praesentium ea doloribus veniam dignissimos asperiores consectetur voluptate repudiandae, eaque amet."
+          />
+          <Post 
+          // in our components we can pass for it what is called 'properties', that you can see kind like 'attributes' in html
+            author="0xREALaldc" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, illum harum perspiciatis sequi facilis vero impedit repellendus placeat. Aspernatur praesentium ea doloribus veniam dignissimos asperiores consectetur voluptate repudiandae, eaque amet."
+          />
+        </main>
+      </div>
     </div>
   )
 }
