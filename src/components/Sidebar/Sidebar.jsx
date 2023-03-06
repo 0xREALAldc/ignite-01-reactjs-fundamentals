2ffc39b1b3c1ebeb54121cacf1764da631ac0c0b
0xREALaldc <andredonat@hotmail.com>
2023-03-06 19:45:10 -0300

    Add: use of Avatar component and removed img tag and styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from '../Avatar/Avatar'
 import styles from './Sidebar.module.css'
 
 export function Sidebar () {
@@ -10,7 +11,7 @@ export function Sidebar () {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/0xREALaldc.png" />
+        <Avatar src="https://github.com/0xREALaldc.png" />
 
         <strong>0xREALaldc</strong>
         <span>Web Developer</span>
