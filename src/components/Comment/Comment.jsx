75e07b9e9d8af0cdcefc00dcc9220b158f9edc4a
0xREALaldc <andredonat@hotmail.com>
2023-03-06 19:45:55 -0300

    Add: the UI and styling for the Comment component

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from '../Avatar/Avatar';
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar 
+        hasBorder={false} 
+        src="https://github.com/diego3g.png" 
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title="03 of March - 15:47h" dateTime="2023-03-05 15:47:53">About 1h ago</time>
+            </div>
+
+            <button title="Delete comment">
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>Wow, awesome project my friend! Congrats!!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Clap <span>23</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
